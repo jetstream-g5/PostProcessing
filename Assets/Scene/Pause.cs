d256921774b2eef82e7efae6bb1acae246a6cf44
Lorenzo <18927@ma-web.nl>
2015-12-14 11:30:46 +0100

    ayylmao

---
+++ Assets/Scene/Pause.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	[SerializeField] private GameObject panel;
+	private bool PauseCheck = false;
+
+	// Use this for initialization
+	void Start () {
+		panel.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (PauseCheck == false) {
+			Time.timeScale = 1;
+			panel.SetActive (false);
+
+			if (Input.GetButtonDown("Start")) {
+				PauseCheck = true;
+			}
+		}
+		 else {
+			Time.timeScale = 0;
+			panel.SetActive (true);
+
+			if (Input.GetButtonDown ("Start")) { 
+				PauseCheck = false;
+			}
+		}
+	}
+}
+
---
+++ Assets/Scene/XboxMovement.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XboxMovement : MonoBehaviour {
+	
+	private Vector3 movementVector;
+	private float movementSpeed = 3;
+	private float jumpPower = 300;
+	private float rotationSpeed = 180;
+	private Rigidbody rb;
+	private bool isGrounded;
+
+	
+	
+	void Start() { 
+		
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Rotate() { 
+		float rightX = Input.GetAxis("RightJoystickX");
+		if (rightX < 0) {
+			transform.Rotate (Vector3.up * Time.deltaTime * -rotationSpeed * -rightX);
+			//Debug.Log(rightX);
+		}
+		if (rightX > 0) {
+			transform.Rotate (Vector3.up * Time.deltaTime * rotationSpeed * rightX);
+			//Debug.Log(rightX);
+		}
+
+
+	}
+
+	void Move(){
+		float forwardY = Input.GetAxis ("LeftJoystickY") * movementSpeed;
+		float forwardX = Input.GetAxis ("LeftJoystickX") * movementSpeed;
+
+		if (forwardY < 0) { 
+			transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed * -forwardY);
+		}
+		if (forwardY > 0) {
+			transform.Translate (Vector3.back * Time.deltaTime * movementSpeed * forwardY);
+		}
+		if (forwardX > 0) { 
+			transform.Translate (Vector3.right * Time.deltaTime * movementSpeed * forwardX);
+		}
+		if (forwardX < 0) { 
+			transform.Translate (Vector3.left * Time.deltaTime * movementSpeed * -forwardX);
+		}
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.tag == "ground") {
+			isGrounded = true;
+		} 
+	}
+	
+	void Update() { 
+
+		Move ();
+		Rotate ();
+		
+		movementVector.y = 0;
+
+		if (Input.GetButtonDown ("A") && isGrounded) { 
+			rb.AddForce(transform.up * jumpPower);
+			//Debug.Log (isGrounded);
+			isGrounded = false;
+		}
+			
+		if (Input.GetButtonDown ("B")) { 
+			Application.LoadLevel("xboxtest");
+		}
+		
+
+
+		
+		
+	}
+}
